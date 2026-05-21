7d05f669ed4138f50d74eb62a69bb6a36292f2b0
Gabriel de Melo Ferreira <gabskael@gmail.com>
2026-05-21 12:12:08 -0300

    fix: adjust min-height and scroll margin in About, Experience, and Intro components for consistency

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -6,7 +6,7 @@ export default function Experience () {
 
     return (
         <>
-            <div id='experience' className='flex flex-col max-w md:flex-row justify-center gap-8 min-h-screen px-4 scroll-mt-20'>
+            <div id='experience' className='flex flex-col max-w md:flex-row justify-center gap-8 min-h-screen px-4 scroll-mt-10'>
                 <div className='flex flex-col gap-20 max-w-200 text-center md:text-left'>
                     <h1 className='text-5xl font-bold text-boldText'>Experience</h1>
                     <div className='flex flex-col gap-3 text-base/6'>
